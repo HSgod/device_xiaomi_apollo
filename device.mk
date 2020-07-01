62d216b592df132ece064a076741198a208a4344
LuK1337 <priv.luk@gmail.com>
2020-07-01 13:18:14 +0200

    apollo: Add product privapp permissions

--- device.mk
+++ device.mk
@@ -48,3 +48,7 @@ PRODUCT_PACKAGES += \
     XiaomiBluetooth \
     XiaomiFrameworks \
     XiaomiSystemUI
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml
