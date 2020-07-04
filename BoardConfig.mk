230e918a448347e0b2eb4cd0ece2f475d6149a41
Davide Garberi <dade.garberi@gmail.com>
2020-07-04 11:00:03 +0200

    apollo: include full qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,13 +124,8 @@ TARGET_COPY_OUT_PRODUCT := product
 BUILD_WITHOUT_VENDOR := true
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
+-include device/qcom/sepolicy/sepolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
 
