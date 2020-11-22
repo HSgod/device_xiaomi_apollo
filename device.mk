95352626ec23d6c5bd34470b2adb194085a45d99
HSgod <hsgodkontakt@gmail.com>
2020-11-22 19:42:02 +0100

    apollo: Inherit dalvik heap configs
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
 
