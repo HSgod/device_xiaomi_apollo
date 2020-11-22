7dbcff868d84bc33a5e4195db551d0b2ca78e50a
HSgod <hsgodkontakt@gmail.com>
2020-11-22 19:42:54 +0100

    apollo: Add device soong namespace
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- device.mk
+++ device.mk
@@ -34,3 +34,7 @@ PRODUCT_PACKAGES += \
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/xiaomi/apollo
