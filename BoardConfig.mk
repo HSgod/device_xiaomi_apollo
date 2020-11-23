78e2432038e7a1eda83890e92bfae2fa967925f9
HSgod <hsgodkontakt@gmail.com>
2020-11-23 10:08:19 +0100

    apollo: Edit some bringup stuff
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,6 @@
 # definition file).
 #
 
-TARGET_BOARD_PLATFORM := kona
 DEVICE_PATH := device/xiaomi/apollo
 
 BUILD_BROKEN_DUP_RULES := true
@@ -37,7 +36,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a75
+TARGET_2ND_CPU_VARIANT := cortex-a76
 
 TARGET_USES_64_BIT_BINDER := true
 
