fc47dfe1ae4aef92762e1a03633a828c02256b33
Bruno Martins <bgcngm@gmail.com>
2020-11-23 09:48:08 +0100

    apollo: Import and bind mount stock custom audio policy config
     * The purpose is to apply the same BT A2DP fix that used to be
       applied in the individual device trees.
    
    Change-Id: I500948b5e1e11ee11097ee2e115b0c23dd808a12

--- device.mk
+++ device.mk
@@ -45,7 +45,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_engine_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_volumes.xml
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+
 
 # Bluetooth
 PRODUCT_PACKAGES += \
