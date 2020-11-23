52f4b98eca4834abbfd26fb4ed18bfd5198b7ddd
HSgod <hsgodkontakt@gmail.com>
2020-11-23 10:01:55 +0100

    apollo: Import stock NFC configs
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_PACKAGES += \
     Tag \
     SecureElement
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # Overlays
 PRODUCT_PACKAGES += \
     XiaomiBluetooth \
